bdf46fb5077c3bd8bea68c03b54e25e361c639ec
deepcloudlabs <deepcloudlabs@gmail.com>
2022-01-18 15:59:52 +0300

    Update Hr.js

--- hr-frontend/src/Hr.js
+++ hr-frontend/src/Hr.js
@@ -11,6 +11,28 @@ class Hr extends React.PureComponent {
         }
     }
 
+    handleInputChange = (event) => {
+        let name = event.target.name;
+        let value = event.target.value;
+        let employee = {...this.state.employee};
+        if (name === "fulltime")
+            employee.fulltime = !employee.fulltime;
+        else
+            employee[name] = value;
+        this.setState({employee})
+    }
+
+    handleFileChange = (event) => {
+        const filename = event.target.files[0];
+        const reader= new FileReader();
+        reader.onload = (e) => {
+            let employee = {...this.state.employee};
+            employee.photo = e.target.result;
+            this.setState({employee})
+        }
+        reader.readAsDataURL(filename);
+    }
+
     render() {
         return (
             <div className="container">
@@ -21,24 +43,44 @@ class Hr extends React.PureComponent {
                     <div className="card-body">
                         <div className="form-group">
                             <label htmlFor="identityNo">Identity No:</label>
-                            <input id="identityNo" type="text"
+                            <input id="identityNo"
+                                   name="identity"
+                                   onChange={this.handleInputChange}
+                                   value={this.state.employee.identity}
+                                   type="text"
                                    className="form-control"/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="name">Full Name:</label>
-                            <input id="name" type="text" className="form-control"/>
+                            <input id="name"
+                                   name="fullname"
+                                   value={this.state.employee.fullname}
+                                   onChange={this.handleInputChange}
+                                   type="text" className="form-control"/>
                         </div>
                         <div className="form-group">
-                            <label htmlFor="salary" >Salary:</label>
-                            <input id="salary" type="text" className="form-control"/>
+                            <label htmlFor="salary">Salary:</label>
+                            <input id="salary"
+                                   name="salary"
+                                   onChange={this.handleInputChange}
+                                   value={this.state.employee.salary}
+                                   type="text" className="form-control"/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="iban">Iban:</label>
-                            <input id="iban" data-bind="value: employee.iban" type="text" className="form-control"/>
+                            <input id="iban"
+                                   name="iban"
+                                   onChange={this.handleInputChange}
+                                   value={this.state.employee.iban}
+                                   data-bind="value: employee.iban" type="text" className="form-control"/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="department">Department:</label>
-                            <select id="department" className="form-control">
+                            <select id="department"
+                                    name="department"
+                                   onChange={this.handleInputChange}
+                                    value={this.state.employee.department}
+                                    className="form-control">
                                 <option>FINANCE</option>
                                 <option>HR</option>
                                 <option>IT</option>
@@ -48,19 +90,31 @@ class Hr extends React.PureComponent {
                         <div className="form-group">
                             <label htmlFor="birthYear">Birth Year:</label>
                             <input id="birthYear"
+                                   name="birthYear"
+                                   onChange={this.handleInputChange}
+                                   value={this.state.employee.birthYear}
                                    type="text"
                                    className="form-control"/>
                         </div>
                         <div className="form-group">
                             <div className="checkbox">
-                                <label><input type="checkbox"></input>Full time?</label>
+                                <label><input
+                                    name="fulltime"
+                                   onChange={this.handleInputChange}
+                                    checked={this.state.employee.fulltime}
+                                    type="checkbox"></input>Full time?</label>
                             </div>
                         </div>
                         <div className="form-group">
                             <label className="control-label" htmlFor="photo">Photo</label>
-                            <img id="photo" className="thumbnail"/>
+                            <img id="photo"
+                                 style={{width: "128px"}}
+                                 src={this.state.employee.photo}
+                                 className="thumbnail"/>
                             <label className="btn btn-info">
-                                <input type="file" style={{display: "none"}}
+                                <input type="file"
+                                       onChange={this.handleFileChange}
+                                       style={{display: "none"}}
                                        className="form-control"/>
                                 <span>File</span>
                             </label>
